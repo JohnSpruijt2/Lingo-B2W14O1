dc803269243823c04e6b58130807596de451b75f
John Spruijt <99058314@mydavinci.nl>
2019-11-20 18:52:32 +0100

    don't know why it works but it works

--- lingo.js
+++ lingo.js
@@ -28,42 +28,62 @@ var checkInput = document.createElement("input")
     page.appendChild(checkInput)
     checkInput.setAttribute('id',"checkInput")
     checkInput.style.marginTop = "50px"
-    checkInput.setAttribute('maxlength',"1")
+    checkInput.setAttribute('maxlength',"5")
     checkInput.setAttribute('onkeydown',"return alphaOnly(event)")
 br()
 var submit = document.createElement("button")
     page.appendChild(submit)
-    submit.innerHTML = "check"
+    submit.innerHTML = "Submit"
     submit.setAttribute('id',"submit")
     submit.setAttribute('onclick',"checkLetter()")
 br()
 br()
-for (i=0; i<4; i++) {
+for (i=0; i<5; i++) {
     var box = document.createElement("div")
         page.appendChild(box)
         box.innerHTML = "?"
         box.setAttribute('class',"box")
         box.setAttribute('id',"box"+i)
 }
-var box1 = document.createElement("div")
-    page.appendChild(box1)
-    box1.innerHTML = "?"
-    box1.setAttribute('class',"box")
+
 
 
 
 function checkLetter() {
+    document.getElementById("wordInput").disabled
     var wordString = document.querySelector('input[id="wordInput"]').value
     var checkString = document.querySelector('input[id="checkInput"]').value
     var splitWord = wordString.split('')
+    var splitCheck = checkString.split('')
+    document.getElementById("wordInput").disabled = true
     for (i=0; i<5; i++) {
-        if (splitWord[i] == checkString) {
-            document.getElementById("box"+i).innerHTML = splitWord[i]
+        document.getElementById("box"+i).style.backgroundColor = "white"
+        if (splitWord[i] != undefined) {
+            if (splitWord[i] == splitCheck[i]) {
+                document.getElementById("box"+i).innerHTML = splitCheck[i]
+                document.getElementById("box"+i).style.backgroundColor = "green"
+            }
+            else if (splitWord[i] != splitCheck[i]) {
+                if (splitCheck[i] == undefined) {
+                    document.getElementById("box"+i).innerHTML = "?"
+                    document.getElementById("box"+i).style.backgroundColor = "white"
+                }
+                else if (splitCheck[i] != undefined) {
+                    for (x=0; x<5; x++) {
+                        if ( splitCheck[i] == splitWord[x]) {
+                            document.getElementById("box"+i).innerHTML = splitCheck[i]
+                            document.getElementById("box"+i).style.backgroundColor = "yellow"
+                        }
+                        else {
+                            document.getElementById("box"+i).innerHTML = splitCheck[i]
+                        }
+                    }
+                }
+            }
         }
     }
-
 }
 function alphaOnly(event) {
     var key = event.keyCode;
-  return ((key >= 65 && key <= 90) || key == 8);
+    return ((key >= 65 && key <= 90) || key == 8);
 }
