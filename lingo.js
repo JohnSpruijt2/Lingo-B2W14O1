a79c5f5c6d0423a4f12e444502e218f7ffc81208
John Spruijt <99058314@mydavinci.nl>
2019-11-20 12:13:06 +0100

    added more stuff

--- lingo.js
+++ lingo.js
@@ -1,21 +1,38 @@
+function br() {
+    var x = document.createElement("a")
+        x.innerHTML = "<br>"
+        page.appendChild(x)
+}
+
 var mainDiv = document.createElement("div")
-    document.body.appendChild(mainDiv);
+    document.body.appendChild(mainDiv)
     mainDiv.setAttribute('id',"page")
+    var page = document.getElementById('page')
 
 var word = document.createElement("a")
 word.innerHTML = "word "
-document.getElementById('page').appendChild(word)
+page.appendChild(word)
 
 var wordInput = document.createElement("input")
-    document.getElementById('page').appendChild(wordInput)
+    page.appendChild(wordInput)
     wordInput.setAttribute('id',"wordInput")
 
-
+br()
 var check = document.createElement("a")
-check.innerHTML = "<br>check "
-document.getElementById('page').appendChild(check)
+check.innerHTML = "check "
+page.appendChild(check)
 
 var checkInput = document.createElement("input")
-    document.getElementById('page').appendChild(checkInput)
+    page.appendChild(checkInput)
     checkInput.setAttribute('id',"checkInput")
     checkInput.style.marginTop = "50px"
+br()
+var submit = document.createElement("button")
+    page.appendChild(submit)
+    submit.innerHTML = "check"
+    submit.setAttribute('id',"submit")
+    submit.setAttribute('onclick',"checkLetter()")
+
+function checkLetter() {
+    alert('')
+}
